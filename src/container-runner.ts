f6940058fc10aba0204b596989a79cbd12d9823e
brookgao <gaojianbo@shizhuang-inc.com>
2026-04-21 14:54:48 +0800

    container-runner: create sync_requests/sync_responses IPC dirs

--- src/container-runner.ts
+++ src/container-runner.ts
@@ -218,6 +218,8 @@ function buildVolumeMounts(
   fs.mkdirSync(path.join(groupIpcDir, 'messages'), { recursive: true });
   fs.mkdirSync(path.join(groupIpcDir, 'tasks'), { recursive: true });
   fs.mkdirSync(path.join(groupIpcDir, 'input'), { recursive: true });
+  fs.mkdirSync(path.join(groupIpcDir, 'sync_requests'), { recursive: true });
+  fs.mkdirSync(path.join(groupIpcDir, 'sync_responses'), { recursive: true });
   mounts.push({
     hostPath: groupIpcDir,
     containerPath: '/workspace/ipc',
